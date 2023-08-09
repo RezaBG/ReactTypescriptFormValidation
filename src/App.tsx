231baa921e13676f336a36a06f560e05d642aba2
rezaBG <rezabarzegargashti@gmail.com>
2023-08-09 16:03:57 +0200

    debug

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import "./App.css";
-import { log } from "console";
 
 type FormData = {
   firstName: string;
@@ -21,12 +20,12 @@ function App() {
       firstName: z.string().min(2).max(30),
       lastName: z.string().min(2).max(30),
       email: z.string().email(),
-      age: z.string().min(18).max(90),
+      age: z.number().min(18).max(70),
       password: z.string().min(5).max(20),
       confirmPassword: z.string().min(5).max(20),
     })
     .refine((data) => data.password === data.confirmPassword, {
-      message: "Password do not match",
+      message: "Passwords do not match",
       path: ["confirmPassword"],
     });
 
@@ -38,30 +37,29 @@ function App() {
     resolver: zodResolver(schema),
   });
 
-  const submitData = (data) => {
+  const submitData = (data: FormData) => {
     console.log("IT WORKED", data);
   };
+
   return (
     <div className="App">
       <form onSubmit={handleSubmit(submitData)}>
-        <label>First Name: </label>
+        <label> First Name: </label>
         <input type="text" {...register("firstName")} />
         {errors.firstName && <span> {errors.firstName.message}</span>}
-
-        <label>Last Name: </label>
+        <label> Last Name: </label>
         <input type="text" {...register("lastName")} />
         {errors.lastName && <span> {errors.lastName.message}</span>}
-
-        <label>Email: </label>
+        <label> Email: </label>
         <input type="email" {...register("email")} />
         {errors.email && <span> {errors.email.message}</span>}
-        <label>Age: </label>
+        <label> Age </label>
         <input type="number" {...register("age", { valueAsNumber: true })} />
-        {errors.age && <span>{errors.age.message}</span>}
-        <label>Password: </label>
+        {errors.age && <span> {errors.age.message}</span>}
+        <label> Password: </label>
         <input type="password" {...register("password")} />
-        {errors.password && <span>{errors.password.message}</span>}
-        <label>Confirm Password: </label>
+        {errors.password && <span> {errors.password.message}</span>}
+        <label> Confirm Password: </label>
         <input type="password" {...register("confirmPassword")} />
         {errors.confirmPassword && (
           <span> {errors.confirmPassword.message}</span>
